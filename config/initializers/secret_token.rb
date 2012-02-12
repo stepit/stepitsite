f28c11794ab08b652bec4aad19e245736487d022
stefi <stefania.panzanini@gmail.com>
2012-02-12 17:00:09 +0100

    commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stepsite::Application.config.secret_token = 'b812fe7fd9bca84c222eb59951ec216bed7ea295bd8be732ac629d2a95c86eb02f5ec1750c37b846c8afc0149b161c43d940e361f28ee42354a51c92e65292fb'
